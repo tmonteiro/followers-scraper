64d144533e0296e37af92a45386eea75e9a64780
Tiago Monteiro <monteiro.tg@gmail.com>
2019-03-21 21:58:59 -0300

    Setup the lowdb

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,34 @@
 import express from 'express';
 import { getTwitterCount, getInstagramCount } from './lib/scraper';
+import db from './lib/db';
 
 const app = express();
 const port = 2090;
 
 app.get('/scrape', async (req, res, next) => {
   console.log('scraping...');
+
   const [tCount, iCount] = await Promise.all([
     getTwitterCount(),
     getInstagramCount(),
   ]);
   res.json({ Twitter: tCount, Instagram: iCount });
+
+  db.get('twitter')
+    .push({
+      date: Date.now(),
+      count: tCount,
+    })
+    .write();
+
+  db.get('instagram')
+    .push({
+      date: Date.now(),
+      count: iCount,
+    })
+    .write();
+
   console.log('done...');
 });
 
-app.listen(port, () => `Server running on port ${port}`);
+app.listen(port, () => console.log(`Server running on port ${port}`));
